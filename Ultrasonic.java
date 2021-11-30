70bb29e1a1bbaf039adc6c21c4054174a0a177ef
Mohamed <mohamedaje@Mohameds-MacBook-Pro.local>
2021-11-30 22:54:47 +0200

    Create Ultrasonic.java

---
+++ Ultrasonic.java
@@ -0,0 +1,21 @@
+package pack;
+
+import lejos.robotics.SampleProvider;
+import lejos.robotics.filter.AbstractFilter;
+
+public class Ultrasonic extends AbstractFilter {
+	  
+	float[] sample;
+
+	  
+
+	    public Ultrasonic(SampleProvider source) {
+	        super(source);
+	        sample = new float[sampleSize];
+	        
+	    }
+	    public float distance() {
+	        super.fetchSample(sample,0);
+	        return sample[0];
+	    }
+}
